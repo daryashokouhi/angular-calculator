930ff9a4e78356e2e11fa02b987e90d19108f06f
daryashokouhi00 <daryashokouhi00@gmail.com>
2019-03-25 11:26:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname+'/dist'));
-
-app.listen(process.env.PORT||8080);
-
-
-//Path Location Strategy
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-
-console.log('Console Listening'); 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))

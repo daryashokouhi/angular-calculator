abe82bd0193a786e36c65aa0d7f3b4268e01eeb8
daryashokouhi00 <daryashokouhi00@gmail.com>
2019-03-25 13:07:11 -0700

    Launching to heroku

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require("express");
+var app = express();
+const path = require('path');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+ app.get("src/index", function(req, res) {
+    console.log("works")
+});
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log("Server started and listening on port " + PORT + ".");
+});
